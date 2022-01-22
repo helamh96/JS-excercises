4f160140b3e0b619f637b1bfbd51e09a9e1ae5c0
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-01-21 18:54:06 -0600

    Delete chapter 9 directory

--- chapter 9/jsonparser.js
+++
@@ -1,9 +0,0 @@
-function dataParse(x){
-    var foo
-    foo=JSON.parse(x);
-    foo.myFn = eval("(" + foo.myFn + ")");
-    return foo;
-}
-
-var str = '{"prop1": 42, "myFn":"function(a,b){return a+b+this.prop1;}"}';
-var obj = dataParse(str);
