01411968f98d3de680e509a29767d1052d5d6e56
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-01-24 13:31:12 -0600

    Create objects.js

---
+++ set 0/Chapter 3/objects.js
@@ -0,0 +1,31 @@
+function printObjProp(x,y){
+    if(y==null){
+        for (let prop in x) {
+            console.log(prop);
+        }
+    }
+    if(y==true){
+        for (let i=0; i<x.length ; i++) {
+            if(typeof x[i] != 'function'){
+                console.log(x[i]);
+            }
+        }
+    }
+    if(y==false){
+        for (let prop in x) {
+            console.log(prop);
+        }
+    }
+    return 0;
+}
+
+
+function CustomObject (a, b) {
+		this.a = a;
+		this.b = b;
+	}
+CustomObject.prototype.c = function () { return this.a + this.b; };
+	var obj = new CustomObject (1, 2);
+	printObjProp (obj); // output: a, b, c
+	printObjProp (obj, false); // output: a, b, c
+	printObjProp (obj, true); // output: a, b
