3cb31eaeb8600ef1fa91f894eeff056305328593
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-01-26 15:36:06 -0600

    Update arraydistance.js

--- set 0/chapter 4/arraydistance.js
+++ set 0/chapter 4/arraydistance.js
@@ -1,28 +1,12 @@
-
 function verifyarray(x,y){
-    if (Array.isArray(x)){
-        if(Array.isArray(y)){
-            if(x.length == y.length){
-                return true;
-            }
-            else{
-                return false
-            }
-        }
-        else{
-            return false
-        }
-    }
-    else{
-        return false
-    }
+    return (Array.isArray(x) && Array.isArray(y) && x.length === y.length)
 }
 
 function distofarray(x,y){
     var sum=0;
     let arr1= [... x];
     let arr2= [... y];
-    for(let i=0; i<=arr1.length - 1; i++){
+    for(let i=0; i<arr1.length ; i++){
        sum+=Math.pow(arr1[i]-arr2[i],2);
     }
     return Math.sqrt(sum);
@@ -32,7 +16,7 @@ function dist(){
     let sum=0;
     for(let i=0; i<(arguments.length/2) ; i++){
         var foo=0;
-        foo=(arguments[i]-arguments[i+arguments.length/2])*(arguments[i]-arguments[i+arguments.length/2]);
+        foo=Math.pow(arguments[i]-arguments[i+arguments.length/2],2)
         sum=foo+sum;
         }
         return Math.sqrt(sum);
@@ -41,21 +25,23 @@ function dist(){
 function distance(){
     switch(true){
         case (arguments.length<=2):
-            if(verifyarray(arguments[0],arguments[1])==true)
+            if(verifyarray(arguments[0],arguments[1]))
             {return distofarray(arguments[0],arguments[1])}
-            else{return "error:incompatible point data"}
+            else{return new Error("Incompatible point data")}
             break;
-        case(arguments.length%2==1):
-            return console.log("insufficient parameters");
+        case(arguments.length%2==1&&arguments.length<6):
+            return new Error("insufficient parameters");
             break;
         case (arguments.length>6):
-            return console.log("error: exceed the parameters");
+            return new Error("exceed the parameters");
             break;
-        case (arguments.length==4 || arguments.length==6):
+        case (arguments.length===4):
+            return dist(arguments[0],arguments[1],arguments[2],arguments[3]);
+            break;
+        case (arguments.length===6):
             return dist(arguments[0],arguments[1],arguments[2],arguments[3],arguments[4],arguments[5]);
             break;
     }
-    return "";
 }
 
 console.log(distance([1,2,3],[1,2,2]));
