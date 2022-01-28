671b95267d6f70ad9ec66184d5b2e3aa3c0aa3a0
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-01-28 14:12:07 -0600

    Update Image.js

--- set 0/Chapter 3/Image.js
+++ set 0/Chapter 3/Image.js
@@ -1,17 +1,17 @@
 function Image(datum, width, heigth, name){
-    this.data=datum;
+    this.dat=datum;
     this.width=width;
     this.heigth=heigth;
     this.name=name;
     this.pixelData = function pix(x,y){
         let newpix= ((x-1)*40+y);
-        return data[newpix];
+        return this.dat[newpix];
     }
 }
 
 var data = new Array(1600).fill("x");
 var img = new Image(data, 40,40,"MyImage");
 img.width;
-img.height;
+img.heigth;
 img.name;
-img.pixelData (20, 4); 
+img.pixelData (20, 4);
