066d4c1cd8e20fef3c028cb2cb8e3914a580c5a3
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-02-01 19:34:55 -0600

    Update holidays.js

--- set 0/chapter 7/holidays.js
+++ set 0/chapter 7/holidays.js
@@ -1,46 +1,20 @@
-function isHoli(x){
-    var abr = x.slice(0,5);
-    switch(abr){
-        case "01/01":
-            console.log("(New years day)");
-            break;
-        case "15/08":
-            console.log("(Assumption of Mary)");
-            break;
-        case "01/11":
-            console.log("(All Saints' day)");
-            break;
-        case "02/11":
-            console.log("(All souls day)");
-            break;
-        case "05/11":
-            console.log("(Day of Guy Fawkes)");
-            break;
-        case "08/12":
-            console.log("(Feast of the Immaculate Conception)");
-            break;
-        case "24/12":
-            console.log("(Christmas Eve)");
-            break;
-        case "25/12":
-            console.log("(Christmas day)");
-            break;
-        case "26/12":
-            console.log("(Boxing day)");
-            break;
-        case "31/21":
-            console.log("(New year's eve)");
-            break;
-    }
-    return;
+let dates = {
+    "01/01":"(New years day)",
+    "15/08":"(Assumption of Mary)",
+    "01/11":"(All Saints' day)",
+    "02/11":"(All souls day)",
+    "05/11":"(Day of Guy Fawkes)",
+    "08/12":"(Feast of the Immaculate Conception)",
+    "24/12":"(Christmas Eve)",
+    "25/12":"(Christmas day)",
+    "26/12":"(Boxing day)",
+    "31/21":"(New year's eve)"
 }
 
 function ChangeDate(x){
     let foo=new Date(x);
     foo=foo.toLocaleDateString('en-GB');
-    console.log(foo);
-    isHoli(foo);
-    return foo;
+    console.log("the date "+x+" in the US format is "+foo+" in the Great Britain "+dates[foo.slice(0,5)]);
 }
 
 ChangeDate("12/25/82");
