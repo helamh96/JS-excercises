de8f00e3f0a40d06edd948fd7fb8befea8d35ba1
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-02-03 18:04:40 -0600

    Update timer.js

--- set 0/chapter 8/timer.js
+++ set 0/chapter 8/timer.js
@@ -1,39 +1,28 @@
-function sleep(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
-}
-
 function a(){
     console.log("a");
-    return 0;
 }
 
 function b(){
     console.log("b");
-    return 0;
 }
 
 function c(){
     console.log("c");
-    return 0;
 }
 
+let t=0;
+    
 function delayer(){
-    for(let timers=0; timers>=0; timers++){
-        sleep(15000);
-        if(timers/2 == Math.floor(timers/2)){
-            a();
-        }
-        if(timers/4==Math.floor(timers/4)){
-            b();
-        }
-        if(timers/5==Math.floor(timers/5)){
-            c();
-        }
+    if(t%2===0){
+        a();
     }
+    if(t%4===0){
+        b();
+    }
+    if(t%5===0){
+        c();
+    }
+    t++;
 }
 
-delayer();
+setInterval(delayer,15000);
