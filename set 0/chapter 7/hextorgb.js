38f7f1f427a9902d49c1060215dc01f37357dd6f
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-02-17 17:26:50 -0600

    Update hextorgb.js

--- set 0/chapter 7/hextorgb.js
+++ set 0/chapter 7/hextorgb.js
@@ -1,5 +1,6 @@
 function hexToRgb(hex){
-    let num = hex.match(/\w{2}/gi);
+    let num = hex.match(/([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})/);
+    num.shift();
     let rgb = "rgb (";
     for (el of num){
         rgb += parseInt(el, 16) +",";
@@ -7,5 +8,6 @@ function hexToRgb(hex){
     rgb = rgb.slice(0, rgb.length-1) +")";
     return rgb;
 }
+
 let number = "#3020ff";
 console.log(hexToRgb(number));
