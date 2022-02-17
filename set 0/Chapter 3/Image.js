891af78ea8fb3ee703dcf0f1f2bf4d1f554f5fe9
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-02-17 13:28:01 -0600

    Update Image.js

--- set 0/Chapter 3/Image.js
+++ set 0/Chapter 3/Image.js
@@ -2,22 +2,27 @@ function Image(datum, width, heigth, name){
     this.dat=datum;
     this.width=width;
     this.heigth=heigth;
-    this.name=name;
+    this.name= name;
     if (width*heigth === datum.length){
             this.dat = datum;
         }
-    else{ 
-            throw new Error("There is no relation with the Array and the width/heigth");
+    else{
+            throw Error("There is no relation with the Array and the width/heigth");
         }
     this.pixelData = function(x,y){
-        let newpix= ((x-1)*this.width+y);
-        return this.dat[newpix];
+        if(x<=this.width&&y<=this.heigth){
+            let newpix = ((x-1)*this.width+y);
+            return this.dat[newpix];
+        }
+        else{
+            throw Error("the current point coordinates are not valid");
+        }
     }
 }
 
 var data = new Array(1600).fill("x");
-var img = new Image(data, 40,40,"MyImage");
-img.width;
+var img = new Image(data, 40, 40, "MyImage");
+img.width
 img.heigth;
 img.name;
 img.pixelData (20, 4);
