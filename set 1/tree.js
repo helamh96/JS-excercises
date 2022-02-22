5fd1c533e754193f29e727d23133be4a81e1f6e9
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-02-22 12:34:28 -0600

    Update tree.js

--- set 1/tree.js
+++ set 1/tree.js
@@ -1,15 +1,20 @@
-let tree = {'A':{'a':{'aa': {}},'b':{'ba':{}, 'bb':{}},'c':{'ca':{}, 'cb':{'cab':{}}}}};
-let branch = [tree];
-let foo = [];
-while (branch.length !== 0){
-    for (b of branch){
-        for (nb in b){
-            console.log(nb);
-            if (Boolean(b[nb])){
-                foo.push(b[nb]);
+function printTree(tree){
+    let branch = [tree];
+    let foo = [];
+    while (branch.length !== 0){
+        for (b of branch){
+            for (nb in b){
+                console.log(nb);
+                if (Boolean(b[nb])){
+                    foo.push(b[nb]);
+                }
             }
         }
+        branch = foo.slice();
+        foo =[];   
     }
-    branch = foo.slice();
-    foo =[];   
 }
+
+let tree1 = {'A':{'a':{'aa': {}},'b':{'ba':{}, 'bb':{}},'c':{'ca':{}, 'cb':{'cab':{}}}}};
+
+printTree(tree1);
