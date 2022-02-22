656c06592f7b47d6c78fada5955d800c8dedfc26
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-02-22 14:16:05 -0600

    Update copyProp.js

--- set 1/copyProp.js
+++ set 1/copyProp.js
@@ -1,7 +1,9 @@
 function copyProp(obj1, obj2, prop){
     if (prop != undefined){
         for (let p of prop){
-            obj1[p] = obj2[p];
+            if(p in obj2){
+                obj1[p] = obj2[p];    
+            }else{throw Error("the property "+p+" doesn't exist in the source object")}
         }
     } else {
         for (let p in obj2){
@@ -12,6 +14,9 @@ function copyProp(obj1, obj2, prop){
 
 let objB={p1:1,p2:2, p3:2};
 let objA={};
+let objC={};
 
-copyProp(objA,objB,["p1"]);
+copyProp(objA,objB,["p1","p3"]);
+console.log(objA);
+copyProp(objC,objB,["p4"]);
 console.log(objA);
