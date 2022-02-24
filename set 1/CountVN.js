1cf4b864b50e7119f32b983a41c9f0b4b3c2b742
helamh96 <94868147+helamh96@users.noreply.github.com>
2022-02-23 19:13:04 -0600

    Update CountVN.js

--- set 1/CountVN.js
+++ set 1/CountVN.js
@@ -1,7 +1,10 @@
 function count(x){
     if (typeof x === "string"){
-        let matching=x.match(/[aeiouAEIOU]/g);
-        return matching.length;
+        let matching=x.match(/[aeiou]/gi);
+        if(matching){
+            return matching.length;
+        }else{return 0}
+        
     }else{
         let num = Math.log10(x);
         return Math.floor(num)+1;
@@ -10,6 +13,8 @@ function count(x){
 
 let a="count the vowels";
 let b=12345678;
+let c="qwrt"
 
 console.log(count(a));
 console.log(count(b));
+console.log(count(c));
